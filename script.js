16a0d68d1da3d94e72ccb1d259779c75480c7628
Saatvik Tikoo <tikoosaatvik@gmail.com>
2024-08-19 01:07:53 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,31 @@
 function retailerSelected(retailer) {
-    let url = '#';
     let event = '';
     switch (retailer) {
-        case 'amazon':
-            url = 'https://www.amazon.com/gp/aws/cart/add.html?ASIN.1=B084QHVL6S&Quantity.1=2&ASIN.2=B0BMFQP2ZZ&Quantity.2=4';
-            event = 'AddToCart';
-            break;
-        case 'target':
-            url = 'https://www.target.com';
-            event = 'ViewContent';
-            break;
-        case 'walmart':
-            url = 'https://affil.walmart.com/cart/addToCart?items=5122403610_2,1378802226_5';
-            event = 'AddToCart';
-            break;
+      case 'amazon':
+        event = 'AddToCart';
+        break;
+      case 'target':
+        event = 'ViewContent';
+        break;
+      case 'walmart':
+        event = 'AddToCart';
+        break;
     }
-    if (url !== '#') {
-        window.open(url, '_blank');
-        fbq('track', event);
+    if (event !== '') {
+      fbq('track', event);
     }
-}
+  }
+
+  function setButtonLinks() {
+    const urls = [
+      'https://www.target.com',
+      'https://affil.walmart.com/cart/addToCart?items=5122403610_2,1378802226_5',
+      'https://www.amazon.com/gp/aws/cart/add.html?ASIN.1=B084QHVL6S&Quantity.1=2&ASIN.2=B0BMFQP2ZZ&Quantity.2=4',
+    ];
+    const buttons = document.getElementsByClassName('button');
+    for (let i = 0; i < buttons.length; i++) {
+      buttons[i].setAttribute('href', urls[i]);
+    }
+  }
+
+  setButtonLinks();
